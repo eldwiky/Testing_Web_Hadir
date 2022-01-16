04fbadca0e703dedaca34a7b89c8bc7a1f808407
eldwiky <kurniawandwiky16@gmail.com>
2022-01-16 21:54:08 +0700

    Dwiky Kurniawan

--- src/main/java/WebHadir/utils/TestCases.java
+++ src/main/java/WebHadir/utils/TestCases.java
@@ -3,7 +3,10 @@ package WebHadir.utils;
 public enum TestCases {
 
 	T1("Testing the authentication login"),
-	T2("Testing the employee reporting");
+	T2("Testing the employee reporting"),
+	T3("Testing form regristration page"),
+	T4("Testing form staff page"),
+	T5("Testing form Register page");
 	
 	private String testName;
 
